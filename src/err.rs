a2feefcb6bc83956a5f5e67f283176bc4b65aa6f
Milan Landaverde <milanlandaverde@gmail.com>
2021-04-21 13:00:21 -0400

    updates ureq dep

--- src/err.rs
+++ src/err.rs
@@ -4,18 +4,18 @@ use std::{error, fmt, io};
 #[non_exhaustive]
 pub enum CliError {
     IO(io::Error),
-    Network,
-    OverwriteFile,
-    Unknown(String)
+    Network(ureq::Error),
+    GitIgnoreNotFound(String),
+    OverwriteFile
 }
 
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             CliError::IO(io_err) => io_err.fmt(f),
-            CliError::Network => write!(f, "Network error"),
+            CliError::Network(network_err) => network_err.fmt(f),
+            CliError::GitIgnoreNotFound(search_keyword) => write!(f, "Gitignore for {} not found", search_keyword),
             CliError::OverwriteFile => write!(f, ".gitignore exists in current working directory. Use `--write-force` to overwrite"),
-            CliError::Unknown(str) => write!(f, "Unknown err: {}", str)
         }
     }
 }
@@ -25,5 +25,11 @@ impl error::Error for CliError {}
 impl From<io::Error> for CliError {
     fn from(err: io::Error) -> Self {
         CliError::IO(err)
+    }
+}
+
+impl From<ureq::Error> for CliError {
+    fn from(err: ureq::Error) -> Self {
+        CliError::Network(err)
     }
 }
--- src/ops.rs
+++ src/ops.rs
@@ -1,9 +1,25 @@
-use crate::err::CliError;
+use serde::{Serialize, Deserialize};
 use std::io::Write;
-use serde::{Deserialize};
+
+use crate::err::CliError;
 
 const REPO_URL: &str = "https://api.github.com/repos/github/gitignore/contents";
 const GITIGNORE_FILE: &str = ".gitignore";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RawGitContent {
+    #[serde(default)]
+    path: Option<String>,
+    #[serde(default)]
+    download_url: Option<String>,
+}
+
+impl RawGitContent {
+    fn list() -> Result<Vec<RawGitContent>, CliError> {
+        let response = ureq::get(REPO_URL).call()?;
+        Ok(response.into_json::<Vec<RawGitContent>>()?)
+    }
+}
 
 struct GitContent {
     path: String,
@@ -18,66 +34,47 @@ impl GitContent {
             .to_lowercase()
             .contains(&lowercase_keyword)
     }
-}
 
-#[derive(Deserialize)]
-struct RawGitContent {
-    #[serde(default)]
-    path: Option<String>,
-    #[serde(default)]
-    download_url: Option<String>,
-}
+    fn list() -> Result<impl Iterator<Item=GitContent>, CliError> {
+        let raw_contents = RawGitContent::list()?;
+        let iter = raw_contents.into_iter().filter_map(|raw| {
+            let path = raw.path.unwrap();
+            if path.len() < 1 || !path.ends_with(".gitignore") {
+                return None;
+            }
 
-fn get_gitignore(keyword: &str) -> String {
-    let contents = list_git_contents();
-    let matched_contents: Vec<GitContent> = contents
-        .filter(|content| content.matches_keyword(keyword))
-        .collect();
+            let download_url = raw.download_url.unwrap();
+            if download_url.len() < 1 {
+                return None;
+            }
 
-    // TODO: handle case where not one or more than one is found
-    let len = matched_contents.len();
-    if len < 1 {
-        eprintln!("Couldn't find one");
-        std::process::exit(1);
-    } else if len > 1 {
-        // TODO: clarify from the user
+            Some(GitContent { path, download_url })
+        });
+        Ok(iter)
     }
 
-    let chosen: &GitContent = &matched_contents[0];
-    let response = ureq::get(&chosen.download_url).call(); // TODO: unwrap
-    return response.into_string().unwrap();
-    // if response.ok() {
-    //     return response.into_string();
-    // }
-    // Err(response.into_synthetic_error()) // TODO: is this what we want?
-}
+    fn get(keyword: &str) -> Result<String, CliError> {
+        let contents = Self::list()?;
+        let matched_contents: Vec<GitContent> = contents
+            .filter(|content| content.matches_keyword(keyword))
+            .collect();
 
-fn list_raw_git_contents() -> Vec<RawGitContent> {
-    let response = ureq::get(REPO_URL).call();
-    return response
-        .into_json_deserialize::<Vec<RawGitContent>>()
-        .unwrap(); // TODO: handle error
-}
-
-fn list_git_contents() -> impl Iterator<Item=GitContent> {
-    let raw_contents = list_raw_git_contents();
-    raw_contents.into_iter().filter_map(|raw| {
-        let path = raw.path?;
-        if path.len() < 1 || !path.ends_with(".gitignore") {
-            return None;
+        // TODO: handle case where not one or more than one is found
+        let len = matched_contents.len();
+        if len < 1 {
+            return Err(CliError::GitIgnoreNotFound(keyword.into()));
+        } else if len > 1 {
+            // TODO: clarify from the user
         }
 
-        let download_url = raw.download_url?;
-        if download_url.len() < 1 {
-            return None;
-        }
-
-        Some(GitContent { path, download_url })
-    })
+        let chosen: &GitContent = &matched_contents[0];
+        let response = ureq::get(&chosen.download_url).call()?;
+        Ok(response.into_string()?)
+    }
 }
 
 pub fn list_all() -> Result<(), CliError> {
-    let list: Vec<String> = list_git_contents().map(|content| content.path).collect();
+    let list: Vec<String> = GitContent::list()?.map(|content| content.path).collect();
     println!(
         "To output a gitignore you can write `{} [keyword]` (i.e. `{} actionscript`)",
         crate::CLI_NAME, crate::CLI_NAME
@@ -91,8 +88,8 @@ pub fn list_all() -> Result<(), CliError> {
 }
 
 pub fn print_single(keyword: &str) -> Result<(), CliError> {
-    let gitignore_contents = get_gitignore(keyword);
-    println!("{}", gitignore_contents);
+    let contents = GitContent::get(keyword)?;
+    println!("{}", contents);
     Ok(())
 }
 
@@ -103,10 +100,9 @@ pub fn write(keyword: &str, force: bool) -> Result<(), CliError> {
     if cwd.exists() && !force {
         return Err(CliError::OverwriteFile);
     } else {
-        let gitignore_contents = get_gitignore(&keyword);
-        println!("{}", gitignore_contents);
-        fs::File::create(cwd)?.write(gitignore_contents.as_bytes())?;
+        let contents = GitContent::get(&keyword)?;
         println!("Writing {} gitignore to .gitignore...", keyword);
+        fs::File::create(cwd)?.write(contents.as_bytes())?;
     }
     Ok(())
 }
